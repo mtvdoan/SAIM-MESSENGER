9ce1b72dc0a39e48338d8aef84d913c918d5773b
Monica Doan <mtvdoan@gmail.com>
2023-02-13 11:41:46 -0800

    update

--- SAIM/client/src/components/LoginPage.jsx
+++ SAIM/client/src/components/LoginPage.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 import {useForm} from 'react-hook-form';
 import {useNavigate} from 'react-router-dom';
 const LoginPage = () => {
-  const navigate = useNavigate();
-  let userData;
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+const navigate = useNavigate();
+    let userData;
+        const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        } = useForm();
 
-  const onSubmit = (data) => {
-    userData = JSON.parse(localStorage.getItem(data.email));
+const onSubmit = (data) => {
+userData = JSON.parse(localStorage.getItem(data.email));
     if (userData) {
-      if (userData.password === data.password) {
-        console.log(userData.screenName + " You Are Successfully Logged In");
-        alert("Thanks for logging in!");
-        navigate("/chat");
-      } else {
-        console.log("Email or Password is not matching with our record");
-      }
-    } else {
-      console.log("Email or Password is not matching with our record");
-    }
-  };
+        if (userData.password === data.password) {
+            console.log(userData.screenName + " You Are Successfully Logged In");
+            alert(`Thanks for logging in ${userData.screenName}`);
+            navigate("/chat");
+        } else {
+            console.log("Email or Password is not matching with our record");
+        }
+        } else {
+            console.log("Email or Password is not matching with our record");
+        }
+};
   return (
     <>
       <p className="title">Login Form</p>
@@ -32,7 +32,9 @@ const LoginPage = () => {
         <input type="email" placeholder="Email" {...register("email", { required: true })} />
         {errors.email && <span style={{ color: "red" }}>
          *Email* is mandatory </span>}
-        <input type="password" placeholder="Password" {...register("password")} />
+        <input type="password" placeholder="Password" {...register("password", {required: true})} />
+        {errors.email && <span style={{ color: "red" }}>
+         *Password* is mandatory </span>}
         <input type={"submit"} style={{ backgroundColor: "#a1eafb" }} />
       </form>
     </>
--- SAIM/client/src/components/UserRegister.jsx
+++ SAIM/client/src/components/UserRegister.jsx
@@ -4,30 +4,35 @@ import { useForm } from 'react-hook-form';
 import {useNavigate} from 'react-router-dom';
 
 const UserRegister = () => {
- const {register, handleSubmit, formState: { errors } } = useForm();
-  const navigate= useNavigate();
-  const onSubmit = (data) => {
-    localStorage.setItem(data.email, JSON.stringify({ 
-        screenName: data.screenName, password: data.password 
-    }));
-    console.log(JSON.parse(localStorage.getItem(data.email)));
-        alert("Thanks for registering!");
-        navigate("/login");
-  };
-  return (
-    <>
-      <p className="title">Registration Form</p>
+    const {register, handleSubmit, formState: { errors } } = useForm();
+    const navigate= useNavigate();
+    const onSubmit = (data) => {
+        localStorage.setItem(data.email, JSON.stringify({ 
+            screenName: data.screenName, password: data.password 
+        }));
+        console.log(JSON.parse(localStorage.getItem(data.email)));
+            alert(`Thanks for registering: ${data.screenName}`);
+            navigate("/login");
+    };
+    return (
+        <>
+            <p className="title">Registration Form</p>
+            <form className="App" onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" placeholder="Screen Name" {...register("screenName", {required: true})} />
+                {errors.screenName && <span style={{ color: "red" }}>
+                *ScreenName* is mandatory </span>}
 
-      <form className="App" onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="Screen Name" {...register("screenName")} />
-        <input type="email" placeholder="email" {...register("email", { required: true })} />
-        {errors.email && <span style={{ color: "red" }}>
-        *Email* is mandatory </span>}
-        <input type="password" placeholder="Password" {...register("password")} />
-        <input type={"submit"} style={{ backgroundColor: "#a1eafb" }} />
-      </form>
-    </>
-  )
+                <input type="email" placeholder="email" {...register("email", { required: true })} />
+                {errors.email && <span style={{ color: "red" }}>
+                *Email* is mandatory </span>}
+
+                <input type="password" placeholder="Password" {...register("password", {required: true})} />
+                {errors.password && <span style={{ color: "red" }}>
+                *Password* is mandatory </span>}
+                <input type={"submit"} style={{ backgroundColor: "#a1eafb" }} />
+            </form>
+        </>
+    )
 }
 
 export default UserRegister
